6689ab6d5323ca974ecfafda0c4f104fef564b02
sahamidi <hamidisa87@gmail.com>
2024-11-14 18:31:58 -0500

    adjusted simpoledata to reflect warranty

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,17 @@
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
-
-    const Warranty = await ethers.getContractFactory("Warranty"); //Change SimpleStorage to Warranty
-    const Warranty = await Warranty.deploy(); //Change SimpleStorage to Warranty
-
-    console.log("Warranty deployed to:", Warranty.address); //Change SimpleStorage to Warranty
-}
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
+  
+    const WarrantyFactory = await ethers.getContractFactory("Warranty"); // Changed to WarrantyFactory to avoid redeclaration
+    const warranty = await WarrantyFactory.deploy(); // Deploying Warranty contract
+    await warranty.deployed();
+  
+    console.log("Warranty contract deployed to:", warranty.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
       console.error(error);
       process.exit(1);
-  });
+    });
